b1e5e109fd6311675589a855d5ca14941a17268a
lianxinbo <lianxinbo@hengli.com>
2023-07-08 13:39:42 +0800

    修复eslint问题

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import viteLogo from './assets/vite.svg';
 import './App.css';
+import base from '@/css/base.module.css';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -27,6 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <p className={base.demo}>CSS</p>
     </>
   );
 }
